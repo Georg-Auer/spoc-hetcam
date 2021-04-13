a7e6d728f1c3929f52af3f5b221e499971182dd3
G-code-SPOC <georg.auer@gmail.com>
2021-04-13 16:28:20 +0200

    4 pic mode

--- app.py
+++ app.py
@@ -197,7 +197,7 @@ def toggled_status():
         print(f"moving time is assumed {moving_time} seconds") 
         task_seperation_increase = moving_time*2
         task_seperation = 1
-        for degree in range(0, 180, 90): # starting angle, stop angle and step angle in degrees
+        for degree in range(0, 360, 90): # starting angle, stop angle and step angle in degrees (180 = picture at 0 & 90, 270 = pic at 0,90,180)
             print(degree)
             schedule_time_movement = schedule_start + timedelta(seconds=task_seperation)
             schedule_time_picture = schedule_start + timedelta(seconds=moving_time+task_seperation)
