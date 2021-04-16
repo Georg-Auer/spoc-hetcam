476377a475b2e80c25f988dbee49223f5c8239b6
Georg1986 <georg.auer@gmail.com>
2021-04-16 14:02:15 +0200

    check if resolution is defined

--- base_camera.py
+++ base_camera.py
@@ -94,7 +94,7 @@ class BaseCamera(object):
     def _thread(cls):
         """Camera background thread."""
         print('Starting camera thread.')
-        resolution = [320, 240]
+        # resolution = [320, 240]
         print(resolution)
         frames_iterator = cls.frames(resolution)
 
